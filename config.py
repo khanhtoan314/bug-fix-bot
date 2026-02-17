71daba7a813f1f40078e0bc6b8a307806649cb57
Khanh Toan Nguyen <khanhtoannguyen314@gmail.com>
2026-02-16 21:29:23 -0800

    Update config.py

--- config.py
+++ config.py
@@ -24,5 +24,5 @@ temperature = 0.1
 Output_Token = 2000
 
 # Settings for the Vertex AI (Currently being used in bug_detection_device.py)
-PROJECT_ID = "even-metrics-480621-g4"
+PROJECT_ID = "vertex-api-key-here"
 LOCATION = "us-central1"
