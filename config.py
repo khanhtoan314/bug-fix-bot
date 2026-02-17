ac676cf3f0fe980e49f08b8776c627bc7b9b8ae9
Khanh Toan Nguyen <khanhtoannguyen314@gmail.com>
2026-02-16 21:21:01 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+"""
+config.py - Configuration file for the bug-fix bot
+
+Function: It stores every settings and functions for the bug-fix bot to function
+
+In essence... this is the 'setting menu' of the bug-fix bot
+
+"""
+
+# Declaring my Google API key
+Google_API_key = "AQ.Ab8RN6IT4Alxq2dAa1WkK90p72_SmXMtza3FG9X_Enw-_1r6QQ"
+
+"""
+This latter section is where the settings of the bot would be defined
+"""
+# Setting the Gemini Model name
+Gemini_Model_name = "gemini-2.5-flash"
+
+# Setting the temperature (paramater of the AI's smart workflow) - Usually between 0 and 1
+temperature = 0.1
+
+# Setting the tokens needed to solve a coding bug - Usually a token = a word
+# Defined 200 Tokens for the bot to solve a coding bug (First fix: 300, Second fix: 2000)
+Output_Token = 2000
+
+# Settings for the Vertex AI (Currently being used in bug_detection_device.py)
+PROJECT_ID = "even-metrics-480621-g4"
+LOCATION = "us-central1"
